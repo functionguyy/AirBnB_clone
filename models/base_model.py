c80771ffb6320c15f2b2322963e017d95d88b49c
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-07 13:46:28 +0100

    feat: update __init__ to take *args and **kwargs
    To create an instance from a dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,22 @@ from datetime import datetime, timezone
 class BaseModel:
     """Defines the internals of a BaseModel object"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize an instance of a BaseModel object type"""
-        current_datetime = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = current_datetime
-        self.updated_at = current_datetime
 
+        if len(kwargs) > 0:
+            datetime_keys = ['updated_at', 'created_at']
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k in datetime_keys:
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, k, v)
+        else:
+            current_datetime = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = current_datetime
+            self.updated_at = current_datetime
 
     def __str__(self):
         """print string representation of instance"""
@@ -23,12 +32,10 @@ class BaseModel:
 
         return "[{:s}] ({:s}) {}".format(class_name, id_, attr_dict)
 
-
     def save(self):
         """update the public instance attribute updated_at with current
         datetime"""
         self.updated_at = datetime.now()
-
 
     def to_dict(self):
         """returns a dictionary containing all key/values of __dict__ of the
@@ -42,7 +49,8 @@ class BaseModel:
         # convert datetime object values to isoformat string
         for k in instance_attr.keys():
             if k in change_keys:
-                new_value = instance_attr[k].isoformat() # convert to isoformat
+                # convert to isoformat
+                new_value = instance_attr[k].isoformat()
                 instance_attr[k] = new_value
 
         return instance_attr
