d155d4e9830f380b1a7e484e88bdb537af0bcd79
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-08 09:51:38 +0100

    fix: link BaseModel to FileStorge properly

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = current_datetime
             self.updated_at = current_datetime
-            storage.new()
+            models.storage.new(self)
 
     def __str__(self):
         """print string representation of instance"""
@@ -38,7 +38,7 @@ class BaseModel:
         """update the public instance attribute updated_at with current
         datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all key/values of __dict__ of the
