88c22058497fd6a0f3338febe8910cf45d9075da
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-08 19:01:51 +0100

    fix: use copy of self.__dict__ in to_dict()
    Using the original causes modification to the attribute dictionary of an
    instance

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Module defines class Base"""
 import uuid
 from datetime import datetime, timezone
-import models
+from models import *
 
 
 class BaseModel:
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = current_datetime
             self.updated_at = current_datetime
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """print string representation of instance"""
@@ -38,12 +38,12 @@ class BaseModel:
         """update the public instance attribute updated_at with current
         datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all key/values of __dict__ of the
         instance"""
-        instance_attr = self.__dict__
+        instance_attr = self.__dict__.copy()
         instance_attr['__class__'] = type(self).__name__
 
         # keys with datetime object values
