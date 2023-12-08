a208013264d717799362e1f27c5c68c7a3b611a5
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-08 19:05:36 +0100

    fix: remove BaseModel import statement

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,6 @@
 """Initialize the models package"""
 from models.engine import file_storage
 
-# explicitly import all model class names to make it available for package
-# modules
-from models.base_model import BaseModel
 
-
-
-models_dict = {"BaseModel": BaseModel}
 storage = file_storage.FileStorage()
 storage.reload()
