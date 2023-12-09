de2705bd7a80fb2de0c999d0fa222bf5ec516c22
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-09 05:43:13 +0100

    feat: create entry point of command interpreter

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module defines custom command line interpreter HBNB"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """define internals of a HBNBCommand instance"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program")
+
+    def do_EOF(self, line):
+        print()
+        return True
+
+    def help_EOF(self):
+        print("Exit the interpreter")
+        print("You can also use the ctrl-D shortcut.")
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
