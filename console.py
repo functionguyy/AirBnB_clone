d305eeeaaa45fe0d33b6946b669213bf8e8b7f5e
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-10 13:29:42 +0100

    feat: add create command to command interpreter

--- console.py
+++ console.py
@@ -14,6 +14,21 @@ class HBNBCommand(cmd.Cmd):
 
     model_classes = MODEL_CLASSES
 
+    def do_create(self, cmd_arg):
+        """
+        create a new instance of BaseModel, saves it to JSON file and prints
+        the id
+        """
+        if cmd_arg in self.model_classes.keys(): 
+            model_cls = self.model_classes[cmd_arg]
+            new_model = model_cls()
+            print("{:s}".format(new_model.id))
+            models.storage.save()
+        elif cmd_arg == "":
+            print("** class name missing **")
+        else:
+            print("** class doesn't exist **")
+
 
     def do_quit(self, cmd_arg):
         return True
